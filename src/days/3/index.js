01c593e2ff3266880d689e529d8ae9c8f6ef2b55
rorz <1853310+rorz@users.noreply.github.com>
2020-12-03 11:04:16 +0000

    day 3

---
+++ src/days/3/index.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+const text = fs.readFileSync(`${__dirname}/input.txt`, "utf-8");
+const treeInput = text.split("\n");
+
+const answer1 = () =>
+  treeInput
+    .map((row, _, arr) => [...Array(arr.length)].map(() => row).join(""))
+    .reduce(
+      (total, currentRow, rowIndex) =>
+        total + +(currentRow[rowIndex * 1] === "#"),
+      0
+    );
+
+const answer2 = () =>
+  [1, 3, 5, 7, 0.5]
+    .map((deltaCol) =>
+      treeInput
+        .map((row, _, arr) =>
+          [...Array(Math.ceil((arr.length / row.length) * deltaCol))]
+            .map(() => row)
+            .join("")
+        )
+        .reduce(
+          (total, currentRow, rowIndex) =>
+            total + +(currentRow[rowIndex * deltaCol] === "#"),
+          0
+        )
+    )
+    .reduce((total, currentCollisions) => total * currentCollisions, 1);
+
+module.exports = { answer1, answer2 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const d1p1 = require("./days/1");
 const d2 = require("./days/2");
+const d3 = require("./days/3");
 
-console.log(d2.answer2());
+console.log(d3.answer2());
