42fc887f92d58cf40d98bbe749adf6f62e2270e8
rorz <1853310+rorz@users.noreply.github.com>
2020-12-05 17:42:04 +0000

    Cleaner Day 5 code

--- src/days/5/index.js
+++ src/days/5/index.js
@@ -3,87 +3,38 @@ const fs = require("fs");
 const input = fs.readFileSync(`${__dirname}/input.txt`, "utf-8");
 const seatCodes = input.split("\n").map((seatNumber) => seatNumber.trim());
 
-// const getNumber = (code, limit, upperChar) =>
-//   [...code].reduce(
-//     ([floor, ceil], char, index) => {
-//       const isUpper = char === upperChar;
-//       if (index === code.length - 1) {
-//         return isUpper ? ceil : floor;
-//       }
-//       return isUpper
-//         ? [floor + Math.ceil((ceil - floor) / 2), ceil]
-//         : [floor, floor + Math.ceil((ceil - floor) / 2)];
-//     },
-//     [0, limit]
-//   );
+const ROW_LEN = 7;
+const COL_LEN = 3;
+const MAX_COL = 7;
 
 const getNumber = (code, upperChar, lowerChar) =>
   parseInt(Number(code.split(upperChar).join(1).split(lowerChar).join(0)), 2);
 
-const answer1 = () => {
-  const seatIds = seatCodes.map((seatCode) => {
-    const rowCode = seatCode.slice(0, 7);
-    const colCode = seatCode.slice(7, 10);
-    const rowNumber = getNumber(rowCode, "B", "F");
-    const colNumber = getNumber(colCode, "R", "L");
-    const seatId = rowNumber * 8 + colNumber;
-    return seatId;
-  });
+const expandSeatCode = (seatCode) => [
+  getNumber(seatCode.slice(0, ROW_LEN), "B", "F"),
+  getNumber(seatCode.slice(-COL_LEN), "R", "L"),
+];
 
-  // return seatIds;s
-  return seatIds.sort((a, b) => b - a)[0];
-};
+const getSeatId = (row, col) => row * 8 + col;
 
-// const padBinary = (string, maxLength) => string.padStart(maxLength, "0");
+const answer1 = () =>
+  seatCodes
+    .map((seatCode) => getSeatId(...expandSeatCode(seatCode)))
+    .sort((a, b) => b - a)[0];
 
-// const answer2 = () => {
-//   // Max column = 119
-//   const allSeatCodes = [...Array(120)]
-//     .map((_, rowNumber) =>
-//       [...Array(8)].map(
-//         (__, colNumber) =>
-//           `${padBinary(rowNumber.toString(2), 7)
-//             .split("0")
-//             .join("F")
-//             .split("1")
-//             .join("B")}${padBinary(colNumber.toString(2), 3)
-//             .split("0")
-//             .join("L")
-//             .split("1")
-//             .join("R")}`
-//       )
-//     )
-//     .flat();
-//   // return seatCodes.filter((seatCode) => allSeatCodes.includes(seatCode)).length;
-//   return seatCodes.length;
-// };
-
-const answer2 = () => {
-  const seatNumbers = seatCodes.map((seatCode) => {
-    const rowCode = seatCode.slice(0, 7);
-    const colCode = seatCode.slice(7, 10);
-    const rowNumber = getNumber(rowCode, "B", "F");
-    const colNumber = getNumber(colCode, "R", "L");
-    return [rowNumber, colNumber];
-  });
-
-  const lowestRow = seatNumbers.sort((a, b) => a[0] - b[0])[0][0];
-  const highestRow = seatNumbers.sort((a, b) => b[0] - a[0]);
-
-  const allSeatNumbers = [...Array(120)]
-    .map((_, rowIndex) =>
-      [...Array(8)].map((__, colIndex) => [rowIndex, colIndex])
-    )
-    .flat()
-    .filter(([rowIndex]) => rowIndex > lowestRow); //FIXME:
-
-  const missingSeatNumber = allSeatNumbers.find(
-    ([rowIndex, colIndex]) =>
-      !seatNumbers.find(([rI, cI]) => rowIndex === rI && colIndex === cI)
-  );
-
-  return missingSeatNumber[0] * 8 + missingSeatNumber[1];
-  // return seatIds.sort((a, b) => b - a)[0];
-};
+const answer2 = () =>
+  [
+    seatCodes
+      .map((seatCode) => expandSeatCode(seatCode))
+      // https://stackoverflow.com/a/35998676/1180964
+      .sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]))
+      .find(
+        ([, colIndex], index, array) =>
+          !(
+            colIndex === MAX_COL ||
+            (colIndex !== MAX_COL && array[index + 1][1] === colIndex + 1)
+          )
+      ),
+  ].map(([rowIndex, colIndex]) => getSeatId(rowIndex, colIndex + 1))[0];
 
 module.exports = { answer1, answer2 };
