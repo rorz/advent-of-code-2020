b7fb2758f24409fba02d276080f04b74b839a3fd
rorz <1853310+rorz@users.noreply.github.com>
2020-12-06 23:58:49 +0000

    This is so stupid...

--- src/index.js
+++ src/index.js
@@ -5,35 +5,47 @@ const days = [...Array(fs.readdirSync(`${__dirname}/days`).length + 1)].map(
   (_, index) => index !== 0 && require(`./days/${index}`)
 );
 
-(async () => {
-  const response = await prompts([
-    {
-      type: "number",
-      name: "day",
-      message: "Which day would you like to run?",
-      validate: (value) => value <= days.length - 1,
-      initial: days.length - 1,
-    },
-    {
-      type: "number",
-      name: "answer",
-      message: "Answer 1 or 2?",
-      validate: (value) => value === 1 || value === 2,
-      initial: days[days.length - 1].answer2 ? 2 : 1,
-    },
-  ]);
-
-  console.log(
-    response.answer === 1
-      ? days[response.day].answer1(
-          days[response.day].processInput(
-            fs.readFileSync(`${__dirname}/inputs/${response.day}.txt`, "utf-8")
-          )
+(async () =>
+  Object.entries(
+    await prompts([
+      {
+        type: "number",
+        name: "day",
+        message: "Which day would you like to run?",
+        validate: (value) => value <= days.length - 1,
+        initial: days.length - 1,
+      },
+      {
+        type: "number",
+        name: "answer",
+        message: "Answer 1 or 2?",
+        validate: (value) => value === 1 || value === 2,
+        initial: days[days.length - 1].answer2 ? 2 : 1,
+      },
+    ])
+  )
+    .sort(([a], [b]) => (a > b ? 1 : -1))
+    .map(([, value]) => value)
+    .forEach(
+      (_, index, response) =>
+        index === response.length - 1 &&
+        console.log(
+          response.answer === 1
+            ? days[response[0]].answer1(
+                days[response[0]].processInput(
+                  fs.readFileSync(
+                    `${__dirname}/inputs/${response[0]}.txt`,
+                    "utf-8"
+                  )
+                )
+              )
+            : days[response[0]].answer2(
+                days[response[0]].processInput(
+                  fs.readFileSync(
+                    `${__dirname}/inputs/${response[0]}.txt`,
+                    "utf-8"
+                  )
+                )
+              )
         )
-      : days[response.day].answer2(
-          days[response.day].processInput(
-            fs.readFileSync(`${__dirname}/inputs/${response.day}.txt`, "utf-8")
-          )
-        )
-  );
-})();
+    ))();
