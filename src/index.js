1a0815fceb2b66fa4e761ea304fb6686028288b7
rorz <1853310+rorz@users.noreply.github.com>
2020-12-06 23:32:38 +0000

    Better prompt / dir handling

--- src/index.js
+++ src/index.js
@@ -1,31 +1,39 @@
 const prompts = require("prompts");
 const fs = require("fs");
 
+const days = [...Array(fs.readdirSync(`${__dirname}/days`).length + 1)].map(
+  (_, index) => index !== 0 && require(`./days/${index}`)
+);
+
 (async () => {
   const response = await prompts([
     {
       type: "number",
       name: "day",
       message: "Which day would you like to run?",
-      // validate: value => value < TOTAL
+      validate: (value) => value <= days.length - 1,
+      initial: days.length - 1,
     },
     {
       type: "number",
       name: "answer",
       message: "Answer 1 or 2?",
       validate: (value) => value === 1 || value === 2,
+      initial: days[days.length - 1].answer2 ? 2 : 1,
     },
   ]);
-  const day = require(`./days/${response.day}`);
-  const input = fs.readFileSync(
-    `${__dirname}/inputs/${response.answer}.txt`,
-    "utf-8"
-  );
-  const processedInput = day.processInput(input);
-  console.log(processedInput);
+
   console.log(
     response.answer === 1
-      ? day.answer1(processedInput)
-      : day.answer2(processedInput)
+      ? days[response.day].answer1(
+          days[response.day].processInput(
+            fs.readFileSync(`${__dirname}/inputs/${response.day}.txt`, "utf-8")
+          )
+        )
+      : days[response.day].answer2(
+          days[response.day].processInput(
+            fs.readFileSync(`${__dirname}/inputs/${response.day}.txt`, "utf-8")
+          )
+        )
   );
 })();
