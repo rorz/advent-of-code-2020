bf972939fcfacb9880bd818c7ccaf8a386703e51
rorz <1853310+rorz@users.noreply.github.com>
2020-12-07 22:16:08 +0000

    Curry Day 6

--- src/days/6.js
+++ src/days/6.js
@@ -1,48 +1,35 @@
 const processInput = (input) => input.split("\n\n");
 
-const sumGroups = (g) => g.reduce((total, count) => total + count, 0);
-
-const answer1 = (groups) =>
-  sumGroups(
-    groups.map((group) =>
+const processGroups = (reducer) => (groups) =>
+  groups
+    .map((group) =>
       group
         .split("\n")
         .map((answer) => [...answer])
-        .reduce(
-          (total, answer, index, arr) =>
-            index === 0
-              ? total + answer.length
-              : total +
-                answer.filter(
-                  (char) => !arr.slice(0, index).flat().includes(char)
-                ).length,
-
-          0
-        )
+        .reduce(reducer, 0)
     )
-  );
+    .reduce((total, count) => total + count, 0);
 
-const answer2 = (groups) =>
-  sumGroups(
-    groups.map((group) =>
-      group
-        .split("\n")
-        .map((answer) => [...answer])
-        .reduce(
-          (total, answer, index, arr) =>
-            index === 0
-              ? total +
-                answer.filter((char) =>
-                  arr
-                    .slice(1, arr.length)
-                    .every((comparisonAnswer) =>
-                      comparisonAnswer.includes(char)
-                    )
-                ).length
-              : total,
-          0
-        )
+const answer1 = processGroups(
+  (total, answer, index, arr) =>
+    total +
+    (index === 0
+      ? answer.length
+      : answer.filter((char) => !arr.slice(0, index).flat().includes(char))
+          .length)
+);
+
+const answer2 = processGroups(
+  (total, answer, index, arr) =>
+    total +
+    +(
+      index === 0 &&
+      answer.filter((char) =>
+        arr
+          .slice(1, arr.length)
+          .every((comparisonAnswer) => comparisonAnswer.includes(char))
+      ).length
     )
-  );
+);
 
 module.exports = { answer1, answer2, processInput };
