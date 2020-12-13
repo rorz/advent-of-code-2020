3658b34121c04325074f32b09448449004e4f6d5
rorz <1853310+rorz@users.noreply.github.com>
2020-12-13 21:52:36 +0000

    Day 10 functional

--- src/days/10.js
+++ src/days/10.js
@@ -4,56 +4,46 @@ const processInput = (input) =>
     .map((jolts) => +jolts)
     .sort((a, b) => a - b);
 
-const answer1 = (adapters) => {
-  const differences = adapters.map((current, index, self) =>
-    index === self.length - 1 ? 3 : self[index + 1] - current
-  );
-  return differences
+const answer1 = (adapters) =>
+  adapters
+    .map((current, index, self) =>
+      index === self.length - 1 ? 3 : self[index + 1] - current
+    )
     .reduce(
       ([ones, threes], current) =>
         current === 1 ? [ones + 1, threes] : [ones, threes + 1],
       [1, 0]
     )
     .reduce((total, current) => total * current, 1);
-};
+
+const options = { 1: [1, 2, 3], 2: [2, 3], 3: [3] };
+const containsOption = (current, search, searchIndex) =>
+  searchIndex > 0 && options[searchIndex].includes(search - current);
 
 const answer2 = (adapters) =>
   [0, ...adapters]
-    .map((current, index, self) => {
-      let numChoices = 0;
-
-      if (index === self.length - 1) {
-        numChoices = 1;
-      }
-      if (
-        self[index + 1] - current === 1 ||
-        self[index + 1] - current === 2 ||
-        self[index + 1] - current === 3
-      ) {
-        numChoices += 1;
-      }
-      if (self[index + 2] - current === 2 || self[index + 2] - current === 3) {
-        numChoices += 1;
-      }
-      if (self[index + 3] - current === 3) {
-        numChoices += 1;
-      }
-
-      return numChoices;
-    })
+    .map((current, index, self) =>
+      self
+        .slice(index, index + 4)
+        .reduce(
+          (total, search, searchIndex) =>
+            total + +containsOption(current, search, searchIndex),
+          0
+        )
+    )
     .reduce(
-      ([total, currentMultiplier], current, index, self) => {
-        const next = self[index + 1];
-        const prev = self[index - 1];
-        if (next == null) return total;
-        if (current === 1) {
-          return [total, 1];
-        }
-        if (next === 1) {
-          return [total * (+(prev > 1 && currentMultiplier - 1) + current), 1];
-        }
-        return [total, +(prev > 1 && currentMultiplier) + current];
-      },
+      ([total, currentMultiplier], current, index, self) =>
+        self[index + 1] == null
+          ? total
+          : [
+              total *
+                (current !== 1 && self[index + 1] === 1
+                  ? +(self[index - 1] > 1 && currentMultiplier - 1) + current
+                  : 1),
+              current === 1 || self[index + 1] === 1
+                ? 1
+                : +(self[index - 1] > 1 && currentMultiplier) + current,
+            ],
       [1, 1]
     );
 
